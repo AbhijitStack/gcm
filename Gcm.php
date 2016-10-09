8d8e56b61558f6c1a19c7242efbc5f9a2fb09328
Abhijit Kumar <abhijit.stack@gmail.com>
2016-10-09 13:53:21 +0530

    git initial commit

---
+++ Gcm.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: abhijit
+ * Date: 10/9/16
+ * Time: 1:35 PM
+ */
+
+include_once('vendor/autoload.php');
+
+class Gcm
+{
+    public function __construct(){
+        echo "Gcm initialize! Testing comporser";
+    }
+
+
+}
